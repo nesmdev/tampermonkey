803eca05e5afa95fc67dbb6f314c9409a6002e22
Ndev <nestormdev@gmail.com>
2024-10-30 20:55:18 -0500

    animeflv online

---
+++ AnimeFLV/animeflv-home.online.js
@@ -0,0 +1,14 @@
+// ==UserScript==
+// @name         AnimeFlvHome
+// @namespace    http://tampermonkey.net/
+// @version      0.1
+// @description  try to take over the world!
+// @author       You
+// @match        https://www3.animeflv.net/
+// @require 	https://cdnjs.cloudflare.com/ajax/libs/moment.js/2.29.1/moment.min.js
+// @require 	https://nesmdev.github.io/ndev/dist/ndev.1.0.1.js
+// @require 	https://nesmdev.github.io/tampermonkey/AnimeFLV/animeflv-home.anime-data.js
+// @require 	https://nesmdev.github.io/tampermonkey/AnimeFLV/animeflv-home.animes-data.js
+// @require 	https://nesmdev.github.io/tampermonkey/AnimeFLV/animeflv-home.js
+// @grant        none
+// ==/UserScript==
